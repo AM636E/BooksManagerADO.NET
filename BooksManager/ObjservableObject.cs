d4011907631c62768a2e9ca4de4aaf80fe148ad2
Nazariy <bnazariy@gmail.com>
2013-11-14 09:07:18 +0200

    Initial commit

---
+++ BooksManager/ObjservableObject.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace BooksManager
+{
+    class ObservableObject : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged = delegate { };
+
+        protected void RaisePropertyChangedEvent(string propertyName)
+        {
+            PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
